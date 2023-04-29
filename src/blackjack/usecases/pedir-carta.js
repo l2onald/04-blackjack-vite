63f6e08740122f54f6dd32ca9111887d30e80dcd
l2onald <diaz-ronald@hotmail.es>
2023-04-29 18:01:10 -0400

    first commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,13 @@
+/**
+ * Permite tomar una carta del deck
+ * @param {Array<String>} deck  Es un arreglo de string
+ * @returns {String} Retorna la carta del deck
+ */
+export const pedirCarta = (deck) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw new Error('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
